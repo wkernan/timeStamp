086d2cf35e0817dbbaf8aa7069a8099ddee85893
Bill <wkernan@gmail.com>
2016-06-04 13:49:04 -0500

    added remove button and functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,20 @@ var rate = 0;
 var monthsWorked = 0;
 var billed = 0;
 
+function changedValue() {
+	var change = new Firebase('https://popping-heat-8389.firebaseio.com/');
+	change.on('value', function(snapshot) {
+
+	})
+}
+
+$(document).on('click', '#remove', function() {
+	var key = $(this).data('key');
+	var id = new Firebase('https//popping-heat-8389.firebaseio.com/' + key);
+	id.remove();
+	$('#' + key).remove();
+})
+
 $('#submit').on('click',function(){
 	name = $('#nameInput').val().trim();
 	startDate = $('#dateInput').val().trim();
@@ -36,7 +50,7 @@ $('#submit').on('click',function(){
 
 flamesBro.on("child_added", function(snapshot){
 	console.log(snapshot.key());
-	$('#newRow').append("<tr><td>" + snapshot.val().name +"</td><td>" + snapshot.val().role + "</td><td>" + snapshot.val().startDate + "</td><td>" + snapshot.val().monthsWorked + ' months' + "</td><td>" + '$ ' + snapshot.val().rate + "</td><td>" + '$ ' + snapshot.val().billed + "</td><td><button class='btn btn-warning' id='edit' data-key='" + snapshot.key() + "'>Edit</button></td><td><button class='btn btn-danger' id='remove' data-key='" + snapshot.key() + "'>X</button></td></tr>" )
+	$('#newRow').append("<tr id='" + snapshot.key() + "'><td>" + snapshot.val().name +"</td><td>" + snapshot.val().role + "</td><td>" + snapshot.val().startDate + "</td><td>" + snapshot.val().monthsWorked + ' months' + "</td><td>" + '$ ' + snapshot.val().rate + "</td><td>" + '$ ' + snapshot.val().billed + "</td><td><button class='btn btn-warning' id='edit' data-key='" + snapshot.key() + "'>Edit</button></td><td><button class='btn btn-danger' id='remove' data-key='" + snapshot.key() + "'>X</button></td></tr>" )
 }, function(errorObject){
 	console.log("Errors handled: " + errorObject.code)
 });
