59a44dce5fa7777420f375bd410d5a9052bf8177
Bill <wkernan@gmail.com>
2016-06-04 13:03:52 -0500

    months and billed functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,41 @@
-var flamesBro = new Firebase('https://popping-heat-8389.firebaseio.com/')
-	var name ="";
-	var startDate = "";
-	var role ="";
-	var rate = 0;
-	var monthsWorked;
-	var billed;
+var flamesBro = new Firebase('https://popping-heat-8389.firebaseio.com/');
+var name ="";
+var startDate = "";
+var role ="";
+var rate = 0;
+var monthsWorked = 0;
+var billed = 0;
 
 $('#submit').on('click',function(){
 	name = $('#nameInput').val().trim();
 	startDate = $('#dateInput').val().trim();
 	rate = $('#rateInput').val().trim();
 	role = $('#roleInput').val().trim();
+	var m = moment(startDate, 'MM-DD-YYYY');
+	console.log(m);
+	monthsWorked = Math.round(moment().diff(m, 'months', true));
+	console.log(Math.round(monthsWorked));
+	billed = monthsWorked * rate;
 	
 	flamesBro.push({
 		name : name,
 		startDate : startDate,
 		rate : rate,
-		role : role
+		role : role,
+		billed: billed,
+		monthsWorked: monthsWorked
 	})
+
+	$('#nameInput').val('');
+	$('#dateInput').val('');
+	$('#roleInput').val('');
+	$('#rateInput').val('');
 
 	return false;
 });
 
 flamesBro.on("child_added", function(snapshot){
-	$('#newRow').append("<tr><td>" + snapshot.val().name +"</td><td>" + snapshot.val().role + "</td><td>" + snapshot.val().startDate + "</td><td>" + snapshot.val().monthsWorked + "</td><td>" + snapshot.val().rate + "</td><td>" + snapshot.val().billed +"</td></tr>" )
+	$('#newRow').append("<tr><td>" + snapshot.val().name +"</td><td>" + snapshot.val().role + "</td><td>" + snapshot.val().startDate + "</td><td>" + snapshot.val().monthsWorked + ' months' + "</td><td>" + '$ ' + snapshot.val().rate + "</td><td>" + '$ ' + snapshot.val().billed + "</td></tr>" )
 }, function(errorObject){
 	console.log("Errors handled: " + errorObject.code)
 });
-
